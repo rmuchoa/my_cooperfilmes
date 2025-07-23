2c5077de42f34653e7ae0472c902720b8d3876a8
Renan Uchôa <renanmarceluchoa@gmail.com>
2025-07-23 12:48:23 -0300

    Initial springboot commit

---
+++ src/main/java/com/brasiliwood/cooperfilmes/CooperfilmesApplication.java
@@ -0,0 +1,13 @@
+package com.brasiliwood.cooperfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperfilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brasiliwood/cooperfilmes/CooperfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasiliwood.cooperfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
